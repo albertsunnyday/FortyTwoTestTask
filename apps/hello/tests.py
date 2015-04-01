59a7067b51cc6e970df03d94c1cead2def17c425
Albert Lee <albertlee@yandex.ru>
2015-04-01 18:05:13 +0300

    t3_middleware: test added

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -10,3 +10,17 @@ class HttpTest(TestCase):
 		self.assertContains(response, '42 Coffee Cups Test Assingment')
 		self.assertContains(response, 'Email: albertlee@yandex.ru')
 		self.assertTemplateUsed(response, 'hello/home.html',)
+
+	def test_middleware(self):
+		self.client.get(reverse('home'))
+		self.client.get(reverse('requests'))
+		req_count = RequestInfo.objects.all().count()
+		req = RequestInfo.objects.get(pk=1)
+		self.assertEqual(req_count, 1)
+		self.assertEqual(req.path, reverse('home'))
+		self.assertEqual(req.method, 'GET')
+		response = self.client.get(reverse('requests'))
+		self.assertEqual(response.status_code, 200)
+		self.assertContains(response, '42 Coffee Cups Test Assingment')
+		self.assertContains(response, reverse('home'))
+		self.assertTemplateUsed(response, 'hello/requests.html',)
