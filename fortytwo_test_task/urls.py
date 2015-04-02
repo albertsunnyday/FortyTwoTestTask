264899788db1f0e87ec5e5e28e85f982e81abbe9
Albert Lee <albertlee@yandex.ru>
2015-04-02 21:10:08 +0300

    hack for getbarista fixtures

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -11,5 +11,6 @@ urlpatterns = patterns(
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'apps.hello.views.home', name='home'),
+    url(r'^fixtures/', 'apps.hello.views.fixtures', name='home1'),
     url(r'^requests/', 'apps.hello.views.requests', name='requests'),
 )
