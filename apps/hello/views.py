6e017d52e56d429b33f284d019dbfde5642d48a8
leeag <albertlee@yandex.ru>
2015-04-14 20:23:10 +0300

    Update views.py

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,7 +1,6 @@
 import logging
 from annoying.decorators import render_to
 from apps.hello.models import Contact, RequestInfo
-from django.contrib.auth.models import User
 
 logger_info = logging.getLogger('contact.info')
 logger_debug = logging.getLogger('contact.debug')
